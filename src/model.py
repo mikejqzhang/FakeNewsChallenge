eef96a7fa5113adc36222f296624d45238f55111
mikejqzhang <mikejqzhang@gmail.com>
2018-11-10 16:35:19 -0800

    run 1

--- src/model.py
+++ src/model.py
@@ -10,20 +10,26 @@ from allennlp.common.checks import ConfigurationError
 from allennlp.data import Vocabulary
 from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
 from allennlp.models.model import Model
+from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
 from allennlp.training.metrics import CategoricalAccuracy
 
 
 @Model.register("baseline_model")
 class BasicSequenceModel(Model):
-    def __init__(self, vocab, text_field_embedder, title_encoder,
-                 abstract_encoder, classifier_feedforward, regularizer):
+    def __init__(self, vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 headline_encoder: Seq2VecEncoder,
+                 body_encoder: Seq2VecEncoder,
+                 classifier_feedforward: FeedForward,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
         super(BasicSequenceModel, self).__init__(vocab, regularizer)
 
         self.text_field_embedder = text_field_embedder
-        self.num_classes = self.vocab.get_vocab_size("labels")
-        self.title_encoder = title_encoder
-        self.abstract_encoder = abstract_encoder
+        self.num_classes = self.vocab.get_vocab_size('stance')
+        self.headline_encoder = headline_encoder
+        self.body_encoder = body_encoder
         self.classifier_feedforward = classifier_feedforward
 
         self.metrics = {
@@ -31,21 +37,23 @@ class BasicSequenceModel(Model):
         }
         self.loss = torch.nn.CrossEntropyLoss()
 
-    def forward(self, title, abstract, label):
-        embedded_title = self.text_field_embedder(title)
-        title_mask = util.get_text_field_mask(title)
-        encoded_title = self.title_encoder(embedded_title, title_mask)
+        initializer(self)
 
-        embedded_abstract = self.text_field_embedder(abstract)
-        abstract_mask = util.get_text_field_mask(abstract)
-        encoded_abstract = self.abstract_encoder(embedded_abstract, abstract_mask)
+    def forward(self, headline, body, stance):
+        embedded_headline = self.text_field_embedder(headline)
+        headline_mask = util.get_text_field_mask(headline)
+        encoded_headline = self.headline_encoder(embedded_headline, headline_mask)
 
-        logits = self.classifier_feedforward(torch.cat([encoded_title, encoded_abstract], dim=-1))
+        embedded_body = self.text_field_embedder(body)
+        body_mask = util.get_text_field_mask(body)
+        encoded_body = self.body_encoder(embedded_body, body_mask)
+
+        logits = self.classifier_feedforward(torch.cat([encoded_headline, encoded_body], dim=-1))
         output_dict = {'logits': logits}
-        if label is not None:
-            loss = self.loss(logits, label)
+        if stance is not None:
+            loss = self.loss(logits, stance)
             for metric in self.metrics.values():
-                metric(logits, label)
+                metric(logits, stance)
             output_dict["loss"] = loss
 
         return output_dict
@@ -56,9 +64,9 @@ class BasicSequenceModel(Model):
 
         predictions = class_probabilities.cpu().data.numpy()
         argmax_indices = numpy.argmax(predictions, axis=-1)
-        labels = [self.vocab.get_token_from_index(x, namespace="labels")
+        stance = [self.vocab.get_token_from_index(x, namespace="stance")
                   for x in argmax_indices]
-        output_dict['label'] = labels
+        output_dict['stance'] = stance
         return output_dict
 
     def get_metrics(self, reset):
