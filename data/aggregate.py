a7685cd113e1d966202a120596943e6a39297dfc
mikejqzhang <mikejqzhang@gmail.com>
2018-12-02 22:38:34 -0800

    fixing data and reorg

--- data/aggregate.py
+++ data/aggregate.py
@@ -1,4 +1,44 @@
 import pickle
 import json
+import numpy as np
+import file_handling as fh
+from tqdm import tqdm
+
+def append_data(split_data, filepath, namespace):
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+    data = data.values.tolist()
+    assert len(split_data) == len(data)
+
+    for ex, d, in tqdm(zip(split_data, data), total=len(data)):
+        ex[namespace] = d
+
+def append_sims(split_data, filepath, namespace):
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+    data = [d.item() for d in data]
+    assert len(split_data) == len(data)
+
+    for ex, d, in tqdm(zip(split_data, data), total=len(data)):
+        ex[namespace] = d
+
+train_data = fh.read_jsonlist('train_data.csv')
+dev_data = fh.read_jsonlist('dev_data.csv')
+test_data = fh.read_jsonlist('test_data.csv')
+
+append_data(train_data, 'train_bodies_senti.pkl', 'body_senti')
+append_data(dev_data, 'dev_bodies_senti.pkl', 'body_senti')
+append_data(test_data, 'test_bodies_senti.pkl', 'body_senti')
 
 
+append_data(train_data, 'train_headline_senti.pkl', 'headline_senti')
+append_data(dev_data, 'dev_headline_senti.pkl', 'headline_senti')
+append_data(test_data, 'test_headline_senti.pkl', 'headline_senti')
+
+append_sims(train_data, 'train_cos_sims.pkl', 'cos_sim')
+append_sims(dev_data, 'dev_cos_sims.pkl', 'cos_sim')
+append_sims(test_data, 'test_cos_sims.pkl', 'cos_sim')
+
+fh.write_jsonlist(train_data, 'train_data.jsonl')
+fh.write_jsonlist(dev_data, 'dev_data.jsonl')
+fh.write_jsonlist(test_data, 'test_data.jsonl')
--- src/dataset_crafted.py
+++ src/dataset_crafted.py
@@ -30,17 +30,26 @@ class FNCCraftedDataSetReader(DatasetReader):
                 data_json = json.loads(line)
                 headline = data_json['headline']
                 body = data_json['body']
+                headline_sentiment = data_json['headline_senti']
+                body_sentiment = data_json['body_senti']
+                tfidf = data_json['cos_sim']
                 stance = data_json['stance']
-                crafted = data_json['crafted']
-                yield self.text_to_instance(headline, body, crafted, stance)
+                yield self.text_to_instance(headline, body, headline_sentiment, body_sentiment, tfidf, stance)
 
-    def text_to_instance(self, headline, body, crafted, stance=None):
+    def text_to_instance(self, headline, body, headline_sentiment, body_sentiment, tfidf, stance=None):
         headline_tokens = self._tokenizer.tokenize(headline)
         body_tokens = self._tokenizer.tokenize(body)
         headline_field = TextField(headline_tokens, self._token_indexers)
         body_field = TextField(body_tokens, self._token_indexers)
-        crafted_field = ArrayField(crafted)
-        fields = {'headline': headline_field, 'body': body_field}
+        headline_sentiment_field = ArrayField(headline_sentiment)
+        body_sentiment_field = ArrayField(body_sentiment)
+        tfidf_field = ArrayField(tfidf)
+        fields = {'headline': headline_field,
+                  'body': body_field
+                  'headline_sentiment': headline_sentiment_field
+                  'body_sentiment': body_sentiment_field
+                  'tfidf': tfidf_field
+                  }
         if stance is not None:
             fields['stance'] = LabelField(stance)
         metadata = {"headline_tokens": [x.text for x in headline_tokens],
